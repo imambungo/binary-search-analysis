f20b9f9155a5e793370f69830667d339c82d19be
M. Imam Pratama <imambungo@gmail.com>
2018-11-08 02:58:12 +0700

    binaryWorm

--- BinaryList.java
+++ BinaryList.java
@@ -10,61 +10,96 @@ class BinaryList {
     }
 
     void doubling() {
-        WormNode pointer = root;
-        while (pointer != null) {
-            if (pointer.left < pointer.right) {
-                if (pointer.left <= pointer.target - 1) {
-                    WormNode newBrother = new WormNode(pointer.left, pointer.target - 1);
+        if (root == null) {
+            System.out.println("nothing to double!");
+        } else {
+            WormNode pointer = root;
+            while (pointer != null) {
+                if (pointer.left < pointer.right) {
+                    if (pointer.left <= pointer.target - 1) {
+                        WormNode newBrother = new WormNode(pointer.left, pointer.target - 1);
+                        if (pointer.prev == null) {
+                            pointer.prev = newBrother;
+                            newBrother.next = pointer;
+                            root = newBrother;
+                        } else {
+                            newBrother.prev = pointer.prev;
+                            newBrother.next = pointer;
+                            newBrother.prev.next = newBrother;
+                            pointer.prev = newBrother;
+                        }
+                    }
+                    if (pointer.target + 1 <= pointer.right) {
+                        pointer.left = pointer.target + 1;
+                        pointer.updateTarget();
+                    }
+                    pointer = pointer.next;
+                } else { // jika tidak bisa lagi mengganda, musnahkan
+                    WormNode theRealNext = pointer.next;
                     if (pointer.prev == null) {
-                        pointer.prev = newBrother;
-                        newBrother.next = pointer;
-                        root = newBrother;
+                        if (pointer.next == null) {
+                            root = null;
+                        } else {
+                            root = pointer.next;
+                            pointer.next = null;
+                            root.prev = null;
+                        }
                     } else {
-                        newBrother.prev = pointer.prev;
-                        newBrother.next = pointer;
-                        newBrother.prev.next = newBrother;
-                        pointer.prev = newBrother;
+                        if (pointer.next == null) {
+                            pointer.prev.next = null;
+                            pointer.prev = null;
+                        } else {
+                            pointer.prev.next = pointer.next;
+                            pointer.next.prev = pointer.prev;
+                            pointer.prev = null;
+                            pointer.next = null;
+                        }
                     }
+                    pointer = theRealNext;
                 }
-                if (pointer.target + 1 <= pointer.right) {
-                    pointer.left = pointer.target + 1;
-                    pointer.updateTarget();
-                }
-                pointer = pointer.next;
-            } else { // jika tidak bisa lagi mengganda, musnahkan
-                WormNode theRealNext = pointer.next;
-                if (pointer.prev == null) {
-                    if (pointer.next == null) {
-                        root = null;
-                    } else {
-                        root = pointer.next;
-                        pointer.next = null;
-                        root.prev = null;
-                    }
-                } else {
-                    if (pointer.next == null) {
-                        pointer.prev.next = null;
-                        pointer.prev = null;
-                    } else {
-                        pointer.prev.next = pointer.next;
-                        pointer.next.prev = pointer.prev;
-                        pointer.prev = null;
-                        pointer.next = null;
-                    }
-                }
-                pointer = theRealNext;
             }
         }
+
     }
 
     void invade() {
         WormNode pointer = root;
         while (pointer != null) {
-            // System.out.print(pointer.left + "-" + pointer.target + "-" + pointer.right + " ");
+            // System.out.print(pointer.left + "-" + pointer.target + "-" + pointer.right +
+            // " ");
             System.out.print(pointer.target + " ");
             pointer = pointer.next;
         }
         System.out.println();
+    }
+
+    void binaryWorm() {
+        if (root == null || root.next != null) {
+            System.out.println("Make sure BinaryList only have 1 node!");
+        } else {
+            int range = root.right + 1;
+            boolean[] position;
+            while (root != null) {
+                position = new boolean[range];
+                WormNode pointer = root;
+                while (pointer != null) {
+                    position[pointer.target] = true;
+                    pointer = pointer.next;
+                }
+                for (int i = 0; i < range; i++) {
+                    if (position[i]) {
+                        System.out.print(i + " ");
+                    } else {
+                        for (int j = 0; j < String.valueOf(i).length(); j++) {
+                            System.out.print(" ");
+                        }
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+                doubling();
+            }
+        }
     }
 
     class WormNode {
--- Main.java
+++ Main.java
@@ -2,14 +2,6 @@ class Main {
     public static void main(String[] args) {
         BinaryList worm = new BinaryList();
         worm.inject(20);
-        worm.invade();
-        worm.doubling();
-        worm.invade();
-        worm.doubling();
-        worm.invade();
-        worm.doubling();
-        worm.invade();
-        worm.doubling();
-        worm.invade();
+        worm.binaryWorm();
     }
 }
